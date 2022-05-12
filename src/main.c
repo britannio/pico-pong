377ca794cd8e0f9efad684935d53e73d256b2c2a
Britannio <britanniojarrett@gmail.com>
2022-05-12 21:25:52 +0100

    Remove old code

--- src/main.c
+++ src/main.c
@@ -5,28 +5,12 @@
 #include "lib/fonts.h"
 #include "lib/st7735.h"
 #include "painting.h"
-#include "logic.h"
 #include "constants.h"
 #include "lib/ICM20948.h"
 #include "pico/multicore.h"
 
-typedef enum
-{
-  Left,
-  Right,
-  Up,
-  Down
-} Move;
-
-void paintGrid();
-void paintCursor();
-void paintHuman(uint8_t pos);
-void paintAI(uint8_t pos);
-void buttonCallback(uint gpio, uint32_t events);
-void updatePosWithMove(Move move);
 void paintGameOverText();
 void startGame();
-
 bool repaintTask();
 bool ballTask();
 bool userPaddleTask();
@@ -37,19 +21,13 @@ void paintAiPaddle();
 void paintUserPaddle();
 void paintDivider();
 
-static int cursorPos = 0;
-
-// Initialise the grid
-GridPos grid[POSITIONS] =
-    {[0 ... LAST_POSITION] = (GridPos){.player = empty, .winningPos = false}};
-
 // Components should only be repainted if they have changed in some way.
 // These flags track this.
 volatile bool userPaddleDirty = true;
 volatile bool aiPaddleDirty = true;
 volatile bool lineDirty = true;
 
-// 0 to 70
+// 0 to MAX_PADDLE_Y
 volatile uint16_t userPaddleY = 0;
 volatile uint16_t aiPaddleY = 35;
 
@@ -99,17 +77,15 @@ void startGame()
 
   // Timers
   // ---------------------------------------------------------------------------
-  const int32_t delay = -16;
+  const int32_t tick = -16;
   struct repeating_timer repaintTimer;
-  add_repeating_timer_ms(delay, repaintTask, NULL, &repaintTimer);
+  add_repeating_timer_ms(tick, repaintTask, NULL, &repaintTimer);
   struct repeating_timer ballTimer;
-  add_repeating_timer_ms(delay, ballTask, NULL, &ballTimer);
+  add_repeating_timer_ms(tick, ballTask, NULL, &ballTimer);
   struct repeating_timer userPaddleTimer;
-  add_repeating_timer_ms(delay, userPaddleTask, NULL, &userPaddleTimer);
+  add_repeating_timer_ms(tick, userPaddleTask, NULL, &userPaddleTimer);
   struct repeating_timer aiPaddleTimer;
-  add_repeating_timer_ms(delay * 2, aiPaddleTask, NULL, &aiPaddleTimer);
-  struct repeating_timer accelTimer;
-  add_repeating_timer_ms(delay, accelerometerTask, NULL, &accelTimer);
+  add_repeating_timer_ms(tick * 4, aiPaddleTask, NULL, &aiPaddleTimer);
 
   while (true)
     tight_loop_contents();
@@ -162,10 +138,7 @@ bool ballTask()
   return true;
 }
 
-bool userPaddleTask()
-{
-  return true;
-}
+
 
 bool aiPaddleTask()
 {
@@ -180,13 +153,11 @@ bool aiPaddleTask()
   {
     // Move paddle up
     aiPaddleDirty = true;
-
     aiPaddleY -= step;
   }
   if (delta < 0 && aiPaddleY + PADDLE_HEIGHT + step <= ST7735_WIDTH)
   {
     // Move paddle down
-    printf("delta-(%d), increasing y\n", delta);
     aiPaddleDirty = true;
     aiPaddleY += step;
   }
@@ -194,7 +165,7 @@ bool aiPaddleTask()
   return true;
 }
 
-bool accelerometerTask()
+bool userPaddleTask()
 {
   float x;
   float y;
@@ -280,146 +251,6 @@ void paintDivider()
 {
   // Line to split the screen
   paintHorizontalLine(ST7735_HEIGHT / 2, 0, ST7735_WIDTH, ST7735_WHITE);
-}
-
-// This method is invoked when the button is pressed.
-// It does the following:
-// 1. Uses the current cursor position to attempt to play a nought.
-// 2. If successful, it repaints the grid then the AI responds with it's own move.
-// 3. If the AI was able to play a move, the grid is repainted once more.
-void buttonCallback(uint gpio, uint32_t events)
-{
-  printf("GPIO %d PRESSED\n", BUTTON_GPIO);
-  // TODO implement buttonCallback()
-}
-
-void updatePosWithMove(Move move)
-{
-  switch (move)
-  {
-  case Left:
-    if (cursorPos % GRID_SIZE == 0)
-    {
-      // Cursor is on the left edge.
-      printf("Rejecting move Left: %d\n", cursorPos);
-      return;
-    }
-    printf("Moving Left\n");
-    cursorPos--;
-    break;
-  case Right:
-    if ((cursorPos + 1) % GRID_SIZE == 0)
-    {
-      // Cursor is on the right edge.
-      printf("Rejecting move Right: %d\n", cursorPos);
-      return;
-    }
-    printf("Moving Right\n");
-    cursorPos++;
-    break;
-  case Up:
-    if (cursorPos < GRID_SIZE)
-    {
-      // Cursor is on the top edge.
-      printf("Rejecting move Up: %d\n", cursorPos);
-      return;
-    }
-    printf("Moving Up\n");
-    cursorPos -= 3;
-    break;
-  case Down:
-    if (cursorPos >= POSITIONS - GRID_SIZE)
-    {
-      // Cursor is on the bottom edge.
-      printf("Rejecting move Down: %d\n", cursorPos);
-      return;
-    }
-    printf("Moving Down\n");
-    cursorPos += 3;
-    break;
-  }
-}
-
-void paintGrid()
-{
-  // Clear top half of screen
-  ST7735_FillRectangle(0, 0, ST7735_WIDTH, ST7735_HEIGHT / 2, ST7735_BLACK);
-
-  // Paint the lines forming the grid
-  const uint16_t oneThird = ST7735_WIDTH / 3;
-  const uint16_t twoThirds = oneThird * 2;
-  paintVerticalLine(oneThird, 0, ST7735_WIDTH, ST7735_WHITE);
-  paintVerticalLine(twoThirds, 0, ST7735_WIDTH, ST7735_WHITE);
-  paintHorizontalLine(oneThird, 0, ST7735_WIDTH, ST7735_WHITE);
-  paintHorizontalLine(twoThirds, 0, ST7735_WIDTH, ST7735_WHITE);
-
-  // Paint the players
-  for (int i = 0; i < POSITIONS; i++)
-  {
-    switch (grid[i].player)
-    {
-    case empty:
-      break;
-    case human:
-      paintHuman(i);
-      break;
-    case ai:
-      paintAI(i);
-      break;
-    }
-  }
-
-  paintCursor();
-}
-
-void paintCursor()
-{
-  // Roughly one third of the display.
-  const uint16_t oneThird = 26;
-
-  // Top left of the cell
-  uint16_t x = cursorPos % 3;
-  x += x * oneThird;
-  uint16_t y = cursorPos / 3;
-  y += y * oneThird;
-
-  // Add inset
-  const uint16_t inset = 12; // Just under half of the box
-  x += inset;
-  y += inset;
-  uint16_t size = 4;
-  ST7735_FillRectangle(x, y, size, size, ST7735_GREEN);
-}
-
-void paintHuman(uint8_t pos)
-{
-  const uint16_t oneThird = 26;
-
-  // Paint a square
-  uint16_t x = (pos % 3);
-  x += x * oneThird;
-  uint16_t y = pos / 3;
-  y += y * oneThird;
-
-  // Add inset
-  const uint16_t inset = 5;
-  x += inset;
-  y += inset;
-  paintSquare(x, y, oneThird - (2 * inset), ST7735_WHITE);
-}
-
-void paintAI(uint8_t pos)
-{
-  const uint16_t oneThird = 26;
-
-  uint16_t x = (pos % 3);
-  x += x * oneThird;
-  uint16_t y = pos / 3;
-  y += y * oneThird;
-
-  const uint16_t inset = 5;
-  paintVerticalLine(x + (oneThird / 2), y + inset, y + oneThird - inset, ST7735_WHITE);
-  paintHorizontalLine(y + (oneThird / 2), x + inset, x + oneThird - inset, ST7735_WHITE);
 }
 
 void paintGameOverText()
